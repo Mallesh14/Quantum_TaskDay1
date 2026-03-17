8c4aa3ea92a2abd502e21a53156a1e83738a7d88
MALLESWARAN P <161807499+Mallesh14@users.noreply.github.com>
2026-03-17 21:21:16 +0530

    Create Twosum.java

---
+++ Twosum.java
@@ -0,0 +1,13 @@
+public class Twosum {
+    public static void main(String[] args) {
+        int[] arr = { 2, 7, 11, 15 };
+        int tar = 9;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == tar) {
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+    }
+}
