b7c65bcabb985d3339ab3f8b6d2a69bda440729c
MALLESWARAN P <161807499+Mallesh14@users.noreply.github.com>
2026-03-17 21:30:00 +0530

    Update missingnumber.java

--- missingnumber.java
+++ missingnumber.java
@@ -1,9 +1,9 @@
 public class missingnumber {
     public static void main(String[] args) {
-        int n=5;
         int[] arr={1,2,4,5};
+        int n=arr[arr.length-1];
         int sum=0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<arr.length;i++)
         {
             sum+=arr[i];
         }
